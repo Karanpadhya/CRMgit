e0cac3dedfb2129602012b24a09c87efe6663d95
karan padhya <padhyakaran@gmail.com>
2021-07-20 20:18:35 -0400

    initial CRM file with some functionalies of view

---
+++ CRM/CRM/CRM/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using CRM.Models;
+
+namespace CRM.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        List<Profile> pr = new List<Profile>()
+        {
+            new Profile() {Id = 1, Fname = "KAran", Lname = "Padhya", Phonenumber = 226, Emailaddress = "Emailadress"},
+            new Profile() {Id = 2, Fname = "Dhruvin", Lname = "kanani", Phonenumber = 226, Emailaddress = "Emailadress"},
+            new Profile() {Id = 3, Fname = "xyz", Lname = "abc", Phonenumber = 226, Emailaddress = "Emailadress"},
+            new Profile() {Id = 4, Fname = "pqr", Lname = "svasd", Phonenumber = 226, Emailaddress = "Emailadress"}
+
+        };
+
+        public ActionResult Index()
+        {
+            ViewBag.data1 = pr;
+            return View();
+        }
+
+        protected override IAsyncResult BeginExecuteCore(AsyncCallback callback, object state)
+        {
+            Thread.CurrentThread.CurrentCulture = Session["Language"] != null
+                ? (Thread.CurrentThread.CurrentUICulture = (CultureInfo)Session["Language"])
+                : (Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US"));
+            return base.BeginExecuteCore(callback, state);
+        }
+
+        public ActionResult Language(string culture)
+        {
+            var lang = new CultureInfo(culture);
+            Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = lang;
+            Session["Language"] = lang;
+
+            if (!(Request.UrlReferrer is null)) return Redirect(Request.UrlReferrer.ToString());
+            return RedirectToAction("index");
+
+        }
+        public ActionResult Edit(int Id=1)
+        {
+            
+            foreach (var var in pr)
+            {
+                if (var.Id == Id)
+                {
+                    return View(var);// matching the data coming from id to the viewbag data
+                }
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Delete(int Id)
+        {
+
+           
+
+            return Index();
+        }
+
+        public ActionResult Gotohome()
+        {
+
+
+
+            return Redirect("~/Home");
+        }
+        
+    }
+}
---
+++ CRM/CRM/CRM/Models/Profile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+using CRM.Resources;
+
+namespace CRM.Models
+{
+    public class Profile : IEnumerable
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(ResourceType = typeof(Resource), Name = "Fname")]
+        public string Fname { get; set; }
+        [Required]
+        [Display(ResourceType = typeof(Resource), Name = "Lname")]
+        public string Lname { get; set; }
+        [Required]
+        [Display(ResourceType = typeof(Resource), Name = "Phonenumber")]
+        public int Phonenumber { get; set; }
+
+        [Required]
+        [Display(ResourceType = typeof(Resource), Name = "Emailaddress")]
+        public string Emailaddress { get; set; }
+
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
